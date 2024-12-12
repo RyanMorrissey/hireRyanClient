9b636046195bcd8652ac251ed4995e24e3bfade5
Ryan Morrissey <ryanmorrissey0@gmail.com>
2024-12-12 14:26:41 -0500

    feat: upgraded from angular 17 to 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/home/home.component';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, HttpClientModule, HomeComponent],
+  imports: [RouterOutlet, NavBarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
