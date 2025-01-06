ca9072cd42349816a1d0e46b00572bec11cfe747
Ryan Morrissey <ryanmorrissey0@gmail.com>
2025-01-06 15:55:11 -0500

    feat: changed up a bunch of home and gallery

--- README.md
+++ README.md
@@ -1,27 +1,10 @@
 # HireRyanClient
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.10.
+Written as a companion to HireRyanServer.
+Built with Angular 19, this was an excuse to learn how to deploy a
+server on AWS S3.
 
-## Development server
-
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+Turns out, building an deploying an angular app by itself is fast and cheap.
+I'd like to mess around with bundling more but at its current scale its not really
+necessary?  Its mostly to give my resume some additional context.  I am still learning,
+so if you have any technical advice, please let me know!
--- src/app/components/shared/image-gallery/image-gallery.component.ts
+++ src/app/components/shared/image-gallery/image-gallery.component.ts
@@ -1,34 +1,52 @@
 import { Component, input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-image-gallery',
   imports: [CommonModule],
   templateUrl: './image-gallery.component.html',
-  styleUrl: './image-gallery.component.less'
+  styleUrls: ['./image-gallery.component.less']
 })
 export class ImageGalleryComponent implements OnInit {
   galleryImages = input.required<string[]>();
-  loadedImages: string[] = []
-
+  loadedImages: string[] = [];
   selectedImage: string | null = null;
-  currentImage: number = 0
-  loadMoreDisabled: boolean = false
+  currentImageIndex: number = 0;
+  showLoadMore: boolean = false;
 
   ngOnInit(): void {
-      this.loadMore()
+    this.loadMore();
   }
 
-  loadMore(): void {
+  async loadMore() {
+    this.showLoadMore = false;
+
     for (let i = 0; i < 10; i++) {
-      this.loadedImages.push(this.galleryImages()[this.currentImage])
-      this.currentImage++
-      // add until we reach the end
-      if (this.currentImage == this.galleryImages().length) {
-        this.loadMoreDisabled = true
+      if (this.currentImageIndex >= this.galleryImages().length) {
         break;
       }
+
+      const imageUrl = this.galleryImages()[this.currentImageIndex];
+      await this.preloadImage(imageUrl);
+      this.loadedImages.push(imageUrl);
+
+      this.currentImageIndex++;
     }
+
+    if (this.currentImageIndex < this.galleryImages().length) {
+      this.showLoadMore = true;
+    }
+  }
+
+  // I ran into an issue of images loading strangely rather than just popping in
+  // This lets me load them one at a time and fully
+  preloadImage(url: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = url;
+      img.onload = () => resolve();
+      img.onerror = () => reject();
+    });
   }
 
   openImage(imageUrl: string): void {
